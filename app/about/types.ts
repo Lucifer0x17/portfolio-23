6760cd4a55c04e40690c91258b3da3b0a0204550
lucifer0x17 <ayush.aa347@gmail.com>
2023-09-09 23:25:51 +0530

    feat: improved code

---
+++ app/about/types.ts
@@ -0,0 +1,17 @@
+export type Experience = {
+    startDate: string;
+    endDate?: string;
+    company: string;
+    title: string;
+    location: string;
+    technologies: string;
+    desc: string[];
+}
+
+export type Education = {
+    startDate: string;
+    endDate?: string;
+    college: string;
+    degree: string;
+    score: string;
+}
