8cf261a69bf61618aff22698fb9b6793f4fa19ca
lucifer0x17 <ayush.aa347@gmail.com>
2023-09-10 21:49:04 +0530

    fix: cursor error

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import sanityClient from "@/sanity/config/client.config";
 import { getProjects } from "@/sanity/sanity.utils";
 import { groq } from "next-sanity";
 import Image from "next/image";
-import Cursor from "./components/Cursor";
-
 const query = groq`*[_type == "projects"]`
 
 
@@ -13,6 +11,6 @@ export default async function Home() {
   console.log("data:",projects)
 
   return <main>
-      <Cursor />
+      Hello world
     </main>;
 }
