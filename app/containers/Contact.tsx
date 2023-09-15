9dc4d67eebd937699b72870d53d4222160dfdfc5
L-Pircy <taveesha.agarwal@gmail.com>
2023-09-16 00:27:33 +0530

    contact section responsive

--- app/containers/Contact.tsx
+++ app/containers/Contact.tsx
@@ -12,46 +12,70 @@ const koulen = Koulen({
 const Contact = () => {
   return (
     <div className="grid grid-cols-1 justify-center items-center">
-      <div className="grid grid-cols-3 text-[210px] font-extrabold tracking-tighter items-end place-items-center px-52 pb-9">
+      <div className="grid grid-cols-3 lg:text-[210px] md:text-[120px] text-[90px] font-extrabold tracking-tighter items-end place-items-center md:px-52 px-12 pb-0">
         <h1 className={`${koulen.className}`}>LET&apos;S</h1>
         <Image
           src={luci}
           height={300}
           width={300}
-          className="rounded-full"
+          className="rounded-full
+          lg:block
+          hidden"
+          alt="No img found"
+        />
+        <Image
+          src={luci}
+          height={210}
+          width={210}
+          className="rounded-full
+          lg:hidden
+          md:block
+          hidden"
+          alt="No img found"
+        />
+        <Image
+          src={luci}
+          height={300}
+          width={300}
+          className="rounded-full
+          lg:hidden
+          md:hidden
+          block"
           alt="No img found"
         />
         <h1 className={`${koulen.className}`}>WORK</h1>
       </div>
-      <h4 className="bg-[#363636] h-fit w-fit text-[27px] font-extralight my-6 justify-self-center self-center mb-9">
+      <h4 className="bg-[#363636] h-fit w-fit md:text-[27px] text-[21px] text-center font-extralight my-6 justify-self-center self-center mb-9">
         Feel Free To Reach Out To Me. I&apos;m Always Open To Discuss New
         Projects.
       </h4>
       {/* socials */}
-      <div className="grid grid-cols-5 px-30 mx-20 mb-12">
+      <div className="grid md:grid-cols-5 grid-cols-1 px-30 mx-20 mb-0">
         <div className="grid grid-cols-4 gap-0 col-span-1 justify-self-center self-center">
           <div className="bg-[#eeeeee] text-[#212121] rounded-full px-3 py-3 h-2 w-2 place-self-end self-center col-span-1"></div>
-          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full text-3xl px-6 py-1 font-extralight col-span-3">
+          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full lg:text-3xl md:text-xl text-xl px-6 py-1 font-extralight col-span-3">
             GitHub
           </span>
         </div>
-        <div className="grid grid-cols-7 gap-0 col-span-3 justify-self-center self-center">
+        <div className="grid grid-cols-7 gap-0 md:col-span-3 col-span-1 justify-self-center self-center">
           <div className="bg-[#eeeeee] text-[#212121] rounded-full px-3 py-3 h-2 w-2 place-self-end self-center col-span-1"></div>
-          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full text-3xl px-6 py-1 font-extralight col-span-6">
+          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full lg:text-3xl md:text-xl text-xl px-6 py-1 font-extralight col-span-6">
             lucifer0x17@gmail.com
           </span>
         </div>
         <div className="grid grid-cols-4 gap-0 col-span-1 justify-self-center self-center">
           <div className="bg-[#eeeeee] text-[#212121] rounded-full px-3 py-3 h-2 w-2 place-self-end self-center col-span-1"></div>
-          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full text-3xl px-6 py-1 font-extralight col-span-3">
+          <span className="hover:bg-[#eeeeee] hover:text-[#212121] hover:rounded-full lg:text-3xl md:text-xl text-xl px-6 py-1 font-extralight col-span-3">
             LinkedIn
           </span>
         </div>
       </div>
-      <h1 className="text-9xl font-extrabold tracking-tighter justify-self-center self-center">
+      <h1
+        className={`lg:text-[210px] md:text-[120px] text-[90px] lg:-mt-10 font-extrabold tracking-tighter justify-self-center self-center ${koulen.className}`}
+      >
         TOGETHER
       </h1>
-      <div className="mt-12 mb-20 py-3 justify-self-center w-fit rounded-full bg-[#8e8e8e] text-4xl text-[#121212] grid grid-cols-4 font-extrabold tracking-tighter gap-0">
+      <div className="mt-0 mb-20 py-3 justify-self-center w-fit rounded-full bg-[#8e8e8e] text-4xl text-[#121212] grid grid-cols-4 font-extrabold tracking-tighter gap-0">
         <Image
           src={calender}
           height={60}
--- app/containers/Education.tsx
+++ app/containers/Education.tsx
@@ -3,11 +3,19 @@ const Education = () => {
     <div className="pt-20">
       <h2 className="text-2xl tracking-[-0.1rem] font-semibold">EDUCATION</h2>
       <div>
-        <div className="grid gap-6 grid-cols-4">
-          <h5 className="text-md font-extralight">Jul 2022 - Aug 2022</h5>
-          <h3 className="text-xl font-light">Manipal University Jaipur</h3>
-          <h4 className="text-xl font-light">B.Tech. Information Technology</h4>
-          <p className="text-sm font-semibold italic text-right">8.93 CGPA</p>
+        <div className="grid md:gap-6 md:grid-cols-8 grid-cols-1">
+          <h5 className="text-md font-extralight md:col-span-2 col-span-2">
+            Jul 2022 - Aug 2022
+          </h5>
+          <h3 className="text-xl font-light md:col-span-2 col-span-1">
+            Manipal University Jaipur
+          </h3>
+          <h4 className="text-xl font-light col-span-3 text-right">
+            B.Tech. Information Technology
+          </h4>
+          <p className="text-sm font-semibold italic text-right md:col-span-1">
+            8.93 CGPA
+          </p>
         </div>
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-400" />
       </div>
